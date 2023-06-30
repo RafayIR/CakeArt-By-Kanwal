ef6b2d21320889c2239a359106746f21c472a434
rafayttg <rafay@thetalentgames.com>
2023-06-30 23:56:51 +0500

    change s in path

--- client/src/components/OrderTab/cakeData.jsx
+++ client/src/components/OrderTab/cakeData.jsx
@@ -3,20 +3,20 @@ const cakeData = [
         name: 'cake1',
         desc: '',
         price: '550',
-        image: 'src/assets/images/cake-img-2.jpg'
+        image: 'assets/images/cake-img-2.jpg'
 
     },
     {
         name: 'cake2',
         desc: '',
         price: '800',
-        image: 'src/assets/images/cake-img-2.jpg'
+        image: 'assets/images/cake-img-2.jpg'
     },
     {
         name: 'cake3',
         desc: '',
         price: '900',
-        image: 'src/assets/images/cake-img-3.jpg'
+        image: 'assets/images/cake-img-3.jpg'
     },
 ]
 
--- client/src/components/OrderTab/cupCakeData.jsx
+++ client/src/components/OrderTab/cupCakeData.jsx
@@ -3,19 +3,19 @@ const cupCakeData = [
         name: 'cup cake Black forest',
         desc: '',
         price: '1000',
-        image: 'src/assets/images/cake-img-1.jpg'
+        image: 'assets/images/cake-img-1.jpg'
     },
     {
         name: 'cup cake Fruits',
         desc: '',
         price: '1200',
-        image: 'src/assets/images/cake-img-1.jpg'
+        image: 'assets/images/cake-img-1.jpg'
     },
     {
         name: 'cup cake 3',
         desc: '',
         price: '850',
-        image: 'src/assets/images/cake-img-1.jpg'
+        image: 'assets/images/cake-img-1.jpg'
     },
 ]
 
