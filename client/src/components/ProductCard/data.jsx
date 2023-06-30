4e053f515477948b53b5c0b6460231c5cfcf63da
Rafay Iqbal <43880610+RafayIR@users.noreply.github.com>
2023-06-30 23:47:16 +0500

    Update data.jsx

--- client/src/components/ProductCard/data.jsx
+++ client/src/components/ProductCard/data.jsx
@@ -19,7 +19,7 @@ const cardData = [
     },
     {
         id : 4,
-        image : '../../assets/images/cake-img-4.jpg',
+        image : '../../../src/assets/images/cake-img-4.jpg',
         title : 'Chocolate Fudge Cake',
         desc : 'This is mast cake , you should try it.'
     }
